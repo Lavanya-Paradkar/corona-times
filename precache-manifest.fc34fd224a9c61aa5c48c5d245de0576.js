14c3583ddc8fe9ed1903946fe447bdfb600c2f46
Lavanya-Paradkar <64947766+Lavanya-Paradkar@users.noreply.github.com>
2020-07-23 11:50:50 +0530

    Updates

---
+++ precache-manifest.fc34fd224a9c61aa5c48c5d245de0576.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c28efb083979221fed3c8d69885579",
+    "url": "/corona-times/index.html"
+  },
+  {
+    "revision": "b698b2b5db670f5f60ef",
+    "url": "/corona-times/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "7960c4e6435175314e8b",
+    "url": "/corona-times/static/css/main.66c34c62.chunk.css"
+  },
+  {
+    "revision": "b698b2b5db670f5f60ef",
+    "url": "/corona-times/static/js/2.adebca8b.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/corona-times/static/js/2.adebca8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7960c4e6435175314e8b",
+    "url": "/corona-times/static/js/main.a02ae97c.chunk.js"
+  },
+  {
+    "revision": "1d0ec23ef8bc3189a17a",
+    "url": "/corona-times/static/js/runtime-main.e4eb1c2c.js"
+  },
+  {
+    "revision": "142c261897e0ee85900b6f4557cf6f73",
+    "url": "/corona-times/static/media/fff.142c2618.JPG"
+  }
+]);
